52b48aeebbddfbe6f163ce2cb96bc41d973c4d43
LuminousLlama <keyushattarde@gmail.com>
2023-12-17 02:09:30 -0800

    goofy spotless formatting

--- src/main/java/org/frcteam2930/common/SubsystemGitSubmodule.java
+++ src/main/java/org/frcteam2930/common/SubsystemGitSubmodule.java
@@ -1,15 +1,14 @@
 package org.frcteam2930.common;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.AutoLogOutput;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 public class SubsystemGitSubmodule extends SubsystemBase {
-    
-    @AutoLogOutput
-    double test = 0.0;
-    @Override
-    public void periodic() {
-        test = 29.30;
-    }
+
+  @AutoLogOutput double test = 0.0;
+
+  @Override
+  public void periodic() {
+    test = 29.30;
+  }
 }
